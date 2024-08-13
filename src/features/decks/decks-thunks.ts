19cc0d2fd722b847191718f6d58c9dcff1bf1803
Vladislava <stdemidovez@gmail.com>
2024-08-13 18:35:21 +0300

    Error handling

--- src/features/decks/decks-thunks.ts
+++ src/features/decks/decks-thunks.ts
@@ -2,6 +2,7 @@ import { Dispatch } from 'redux'
 import { decksAPI, UpdateDeckParams } from './decks-api.ts'
 import { addDeckAC, deleteDeckAC, setDecksAC, updateDeckAC } from './decks-reducer.ts'
 import { setAppStatusAC } from '../../app/app-reducer.ts'
+import { isAxiosError } from 'axios'
 
 export const fetchDecksTC = () => async (dispatch: Dispatch) =>  {
   dispatch(setAppStatusAC('loading'))
@@ -27,8 +28,24 @@ export const deleteDeckTC = (id: string) => async (dispatch: Dispatch) => {
   })
 }
 
+//case-1: ошибка бэкенда. Ошибку создает axios, в e.response.data помещает ответ сервера
+//case-2: network error - axios создает объект ошибки, сообщение можно взять из поля e.message
+//case-3: синхронные ошибки - создается "нативная" JS-ошибка, имеет поле message
 export const updateDeckTC = (params: UpdateDeckParams) => async (dispatch: Dispatch) => {
-  return decksAPI.updateDeck(params).then((res) => {
+  try {
+    const res = await decksAPI.updateDeck(params)
     dispatch(updateDeckAC(res.data))
-  })
+  } catch (e){
+    let errorMessage: string;
+    if(isAxiosError<ServerError>(e)){
+      errorMessage = e.response ? e.response.data.errorMessages[0].message : e.message
+    } else {
+      errorMessage = (e as Error).message;
+    }
+    console.log(errorMessage);
+  }
+}
+
+type ServerError = {
+  errorMessages: Array<{field: string, message: string}>
 }
